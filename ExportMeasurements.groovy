60b9dd908d5c58df738e8fefb9cf3020ae04d8b4
Lilly-Flore <74827392+Lilly-Flore@users.noreply.github.com>
2023-12-22 19:31:07 +0100

    Add files via upload

--- ExportMeasurements.groovy
+++ ExportMeasurements.groovy
@@ -14,7 +14,7 @@ def separator = "\t"
 
 // Choose the columns that will be included in the export
 // Note: if 'columnsToInclude' is empty, all columns will be included
-def columnsToInclude = new String[]{"Name", "Number of nuclei", 
+def columnsToInclude = new String[]{"Image", "Object ID", "Name", "Number of nuclei", 
                                     "Area µm^2", "Nuclei Area µm^2: Mean", "Area Ratio %",
                                     "Max diameter µm", "Nuclei diameter µm: Mean Max", "Diameter Ratio %: Max",
                                     "Min diameter µm", "Nuclei diameter µm: Mean Min", "Diameter Ratio %: Min", 
